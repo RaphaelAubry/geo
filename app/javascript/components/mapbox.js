127e0779f087305e6c8929348a81c15be4132bb7
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-03-01 15:22:03 +0100

    javascript extraction

--- app/javascript/components/mapbox.js
+++ app/javascript/components/mapbox.js
@@ -10,6 +10,7 @@ const initMap = () => {
   });
 
   const coordinates = [[9, 46],[14, 46],[14, 42],[8, 43]]
+  console.log(coordinates)
   map.on('load', () => {
     // Add a data source containing GeoJSON data.
     map.addSource('maine', {
@@ -50,11 +51,44 @@ const initMap = () => {
       }
     });
   });
-
+extractCoord();
 }
 
 const extractCoord = () => {
+  const polygons = document.querySelectorAll(".polygon")
+  polygons.forEach((polygon => {
+    console.log(polygon.dataset.polygon)
 
+    const coord = polygon.dataset.polygon
+    const regex = /(POLYGON [()]{2}|[))]{2})|[,]/mg;
+    const subst = ``;
+    const result = coord.replace(regex, subst);
+    console.log(result)
+
+
+    const regex2 = /[,]/g;
+    const subst2 = ``;
+    const result2 = result.replace(regex2, subst2);
+    console.log(result2)
+
+    const regex3 = /([0-9]*[.][0-9] [0-9]*[.][0-9])/g;
+    const subst3 = ``;
+
+    const finals = []
+
+    //loop in the
+    const text = result2.match(regex3)
+
+    console.log(text[0])
+    finals.push(text[0])
+    console.log(finals)
+
+    result2.replace(regex3, subst3);
+
+    console.log(result2)
+
+
+  }))
 }
 
 export { initMap }
